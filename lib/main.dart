c754f89e1ca63d81f8ee480426373028f32c49e9
Manuel López <thedarkymagic@gmail.com>
2020-01-12 23:11:42 -0300

    Deleted transparent status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:github_app/src/app.dart';
 
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-  runApp(App());
-} 
+void main() => runApp(App());
